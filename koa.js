34443fe5a702dfe579c340b0b9e10e0c27df6f5b
Ali G <ali.glenesk@gmail.com>
2017-11-10 08:04:43 -0800

    Working on routes

--- koa.js
+++ koa.js
@@ -1,11 +1,50 @@
-var Koa = require('koa');
+// var Koa = require('koa');
+// var route = require('koa-route');
 
-var app = new Koa();
+// var app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
-app.listen(8008);
+// app.use(route.get('/', index));
+// app.use(route.get('/about', about));
 
-console.log('Koa is listening on port 8008');
+// //The asterisk is key, designates a function as a generator.
+// function *index() {
+//   this.body = "<h1>Hello! This is my home page!</h1>";
+// }
+
+// function *about() {
+//   this.body = "<h2>My name is Ali and I like JavaScript</h2>";
+// }
+
+// app.listen(8008);
+// console.log('Koa is listening on port 8008');
+
+
+const _ = require('koa-route');
+const Koa = require('koa');
+const app = new Koa();
+
+const db = {
+  tobi: { name: 'tobi', species: 'ferret' },
+  loki: { name: 'loki', species: 'ferret' },
+  jane: { name: 'jane', species: 'ferret' }
+};
+
+const pets = {
+  list: (ctx) => {
+    const names = Object.keys(db);
+    ctx.body = 'pets: ' + names.join(', ');
+  },
+
+  show: (ctx, name) => {
+    const pet = db[name];
+    if (!pet) return ctx.throw('cannot find that pet', 404);
+    ctx.body = pet.name + ' is a ' + pet.species;
+  }
+};
+
+app.use(_.get('/pets', pets.list));
+app.use(_.get('/pets/:name', pets.show));
+
+app.listen(3000);
+console.log('listening on port 3000');
