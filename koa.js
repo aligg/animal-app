9d6cf2ac72d06b9d7df267304bed0b46bb8576ce
Ali G <ali.glenesk@gmail.com>
2017-11-10 09:32:51 -0800

    Still trying to get something to work

--- koa.js
+++ koa.js
@@ -1,50 +1,86 @@
-// var Koa = require('koa');
-// var route = require('koa-route');
 
-// var app = new Koa();
+/////////////////////////////////////////////////////////////
+// const http = require('http');
+// const https = require('https');
+// const Koa = require('koa');
+// const app = new Koa();
+
+// // x-response-time
+// app.use(async (ctx, next) => {
+//   const start = Date.now();
+//   await next();
+//   const ms = Date.now() - start;
+//   ctx.set('X-Response-Time', `${ms}ms`);
+// });
+
+// // logger
+// app.use(async (ctx, next) => {
+//   const start = Date.now();
+//   await next();
+//   const ms = Date.now() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+// });
+
+// // response
+
+// app.use(async ctx => {
+//   ctx.body = 'Hello World';
+// });
+
+// app.listen(3000);
+// console.log('Koa is listening on port 3000');
 
 
-// app.use(route.get('/', index));
-// app.use(route.get('/about', about));
 
-// //The asterisk is key, designates a function as a generator.
-// function *index() {
-//   this.body = "<h1>Hello! This is my home page!</h1>";
-// }
+/////////////////////////////////////////////////////////////
+var Koa = require('koa');
+var route = require('koa-route');
 
-// function *about() {
-//   this.body = "<h2>My name is Ali and I like JavaScript</h2>";
-// }
+var app = new Koa();
 
-// app.listen(8008);
-// console.log('Koa is listening on port 8008');
+app.use(route.get('/', index));
+app.use(route.get('/about', about));
+
+//The asterisk is key, designates a function as a generator.
+function *index() {
+  this.body = "<h1>Hello! This is my home page!</h1>";
+}
+
+function *about() {
+  this.body = "<h2>My name is Ali and I like JavaScript</h2>";
+}
+
+app.listen(8008);
+console.log('Koa is listening on port 8008');
 
 
-const _ = require('koa-route');
-const Koa = require('koa');
-const app = new Koa();
 
-const db = {
-  tobi: { name: 'tobi', species: 'ferret' },
-  loki: { name: 'loki', species: 'ferret' },
-  jane: { name: 'jane', species: 'ferret' }
-};
+/////////////////////////////////////////////////////////////
+// const _ = require('koa-route');
+// const Koa = require('koa');
+// const app = new Koa();
 
-const pets = {
-  list: (ctx) => {
-    const names = Object.keys(db);
-    ctx.body = 'pets: ' + names.join(', ');
-  },
+// const db = {
+//   tobi: { name: 'tobi', species: 'ferret' },
+//   loki: { name: 'loki', species: 'ferret' },
+//   jane: { name: 'jane', species: 'ferret' }
+// };
 
-  show: (ctx, name) => {
-    const pet = db[name];
-    if (!pet) return ctx.throw('cannot find that pet', 404);
-    ctx.body = pet.name + ' is a ' + pet.species;
-  }
-};
+// const pets = {
+//   list: (ctx) => {
+//     const names = Object.keys(db);
+//     ctx.body = 'pets: ' + names.join(', ');
+//   },
 
-app.use(_.get('/pets', pets.list));
-app.use(_.get('/pets/:name', pets.show));
+//   show: (ctx, name) => {
+//     const pet = db[name];
+//     if (!pet) return ctx.throw('cannot find that pet', 404);
+//     ctx.body = pet.name + ' is a ' + pet.species;
+//   }
+// };
 
-app.listen(3000);
-console.log('listening on port 3000');
+// app.use(_.get('/pets', pets.list));
+// app.use(_.get('/pets/:name', pets.show));
+
+// app.listen(3000);
+// console.log('listening on port 3000');
