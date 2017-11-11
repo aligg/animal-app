f0f19919232c1d6e2b0f8bf4afd61eadb3326b39
Ali G <ali.glenesk@gmail.com>
2017-11-11 08:29:30 -0800

    Still working on koa

--- koa.js
+++ koa.js
@@ -1,4 +1,17 @@
 
+var koa = require('koa');
+var app = new koa();
+ 
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+ 
+app.listen(3000);
+console.log('Koa is listening on port 3000');
+
+
+
+
 /////////////////////////////////////////////////////////////
 // const http = require('http');
 // const https = require('https');
@@ -33,25 +46,25 @@
 
 
 /////////////////////////////////////////////////////////////
-var Koa = require('koa');
-var route = require('koa-route');
+// var Koa = require('koa');
+// var route = require('koa-route');
 
-var app = new Koa();
+// var app = new Koa();
 
-app.use(route.get('/', index));
-app.use(route.get('/about', about));
+// app.use(route.get('/', index));
+// app.use(route.get('/about', about));
 
-//The asterisk is key, designates a function as a generator.
-function *index() {
-  this.body = "<h1>Hello! This is my home page!</h1>";
-}
+// //The asterisk is key, designates a function as a generator.
+// function *index() {
+//   this.body = "<h1>Hello! This is my home page!</h1>";
+// }
 
-function *about() {
-  this.body = "<h2>My name is Ali and I like JavaScript</h2>";
-}
+// function *about() {
+//   this.body = "<h2>My name is Ali and I like JavaScript</h2>";
+// }
 
-app.listen(8008);
-console.log('Koa is listening on port 8008');
+// app.listen(8008);
+// console.log('Koa is listening on port 8008');
 
 
 
