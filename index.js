7df067d07e3baa09690b325b1e2fedc04147a2ff
Ali G <ali.glenesk@gmail.com>
2017-11-12 16:27:01 -0800

    code cleanup

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// const path = require('path');
 const mount = require('koa-mount'); //using mount to enable modular code
 const _ = require('koa-route'); //enable routes
-// const views = require('koa-views'); //
 const Koa = require('koa');
 const app = new Koa();
 
@@ -9,7 +7,6 @@ const app = new Koa();
 let animalApp = require('./components/animals.js');
 let home = require('./components/home.js')
 
-// app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }));
 
 app.use(mount('/animals', animalApp));
 app.use(mount('/', home));
