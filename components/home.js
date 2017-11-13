175b682c319c34222127c4ce8dd1c922bd58585a
Ali G <ali.glenesk@gmail.com>
2017-11-12 16:25:32 -0800

    Working on views

--- components/home.js
+++ components/home.js
@@ -1,14 +1,24 @@
+const path = require('path');
+const views = require('koa-views'); //
 var koa = require('koa');
 const _ = require('koa-route');
 var app = module.exports = new koa();
+app.use(views(path.join(__dirname, '../views'), { extension: 'ejs' }));
 
 
-const home = {
-    display: (ctx) => {
-        ctx.type = 'html';
-        ctx.body = 'Check out our <a href="/animals">animals!</a><br><img src="http://www.androidguys.com/wp-content/uploads/2015/11/02e3f64f32bfffc28b94cd7f53cf3fe0.jpg" style="width:100%;">'
-        ;
-    }
-};
+// const home = {
+//     display: (ctx) => {
+//         ctx.type = 'html';
+//         ctx.body = 'Check out our <a href="/animals">animals!</a><br><img src="http://www.androidguys.com/wp-content/uploads/2015/11/02e3f64f32bfffc28b94cd7f53cf3fe0.jpg" style="width:100%;">'
+//         ;
+//     }
+// };
 
-app.use(_.get('/', home.display));
+app.use(async function(ctx) {
+  await ctx.render('home', {  });
+});
+
+
+
+
+// app.use(_.get('/', home.display));
